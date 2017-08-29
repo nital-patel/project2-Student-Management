1933c89f246ad91e10c41d8457def2fa9bc47e42
Nital Patel <patelnital36@gmail.com>
2017-08-29 14:21:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Student!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App is up and running. Listening on port ${PORT}`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const studentRouter = require('./routes/student-routes');
+app.use('/movies', moviesRouter)
