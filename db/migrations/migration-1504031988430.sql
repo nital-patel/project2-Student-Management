f1e64c480680f2b75af073a2843285dd2813779d
Nital Patel <patelnital36@gmail.com>
2017-08-29 16:20:47 -0400

    Add migration to create movies table and movies seed

---
+++ db/migrations/migration-1504031988430.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS students (
+  id BIGSERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT
+);
---
+++ db/seeds/seed.sql
@@ -0,0 +1,32 @@
+INSERT INTO students (student_id, name, course, is_passing, comment) VALUES
+(1,
+'Jöhnny Ãpplesēēd',
+'WDI',
+'t',
+'Knows a little about apples.'
+),
+
+(
+2,
+'Dennis Ritchie',
+'UXI',
+'t',
+'Knows something about programming.'
+),
+
+(
+3,
+'Håkon Wium Lie',
+'UXI',
+'f',
+'Knows something about CSS.'
+),
+
+(
+4,
+'Brendan Eich',
+'WDI',
+'t',
+'Knows something about JavaScript.'
+);
+
